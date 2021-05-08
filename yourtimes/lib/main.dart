6e874b87e2694d4727c132961a3ccba921eeb2b4
abdosabaa7 <abdosaba21@yahoo.com>
2021-05-09 01:18:46 +0200

    frist commit

---
+++ yourtimes/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'splashscreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'YOURTIME',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ yourtimes/lib/splashscreen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'homepage';
+
+class SplashScreen extends StatelessWidget {
+
+  pushToHomePage(BuildContext context) async{
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder:(context)=>HomePage()));
+  }
+  @override
+  Widget build(BuildContext context) {
+    pushToHomePage(context);
+    return Scaffold(
+
+      backgroundColor: Colors.white,
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.check_circle,size: 150,color: Color(0xff30384c),),
+              SizedBox(height: 10,),
+              Text('Your-Time App',style: TextStyle(color: Color(0xff30384c),fontSize: 40,fontWeight: FontWeight.bold),)
+            ],
+          )
+      ),
+    );
+  }
+}
